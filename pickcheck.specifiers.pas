1ec7399847f5276fc1dd10ccdf92a29071ea9947
Brian Olsen <brian@bolsen.org>
2025-09-08 11:41:13 +0200

    Specifiers update

--- pickcheck.specifiers.pas
+++ pickcheck.specifiers.pas
@@ -10,6 +10,27 @@ interface
 uses
   cthreads, Classes, SysUtils, StrUtils, Generics.Collections, PickCheck;
 
+const
+    Primes: array of Integer = (
+    2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
+    31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
+    73, 79, 83, 89, 97, 101, 103, 107, 109, 113,
+    127, 131, 137, 139, 149, 151, 157, 163, 167, 173,
+    179, 181, 191, 193, 197, 199, 211, 223, 227, 229,
+    233, 239, 241, 251, 257, 263, 269, 271, 277, 281,
+    283, 293, 307, 311, 313, 317, 331, 337, 347, 349,
+    353, 359, 367, 373, 379, 383, 389, 397, 401, 409,
+    419, 421, 431, 433, 439, 443, 449, 457, 461, 463,
+    467, 479, 487, 491, 499, 503, 509, 521, 523, 541,
+    547, 557, 563, 569, 571, 577, 587, 593, 599, 601,
+    607, 613, 617, 619, 631, 641, 643, 647, 653, 659,
+    661, 673, 677, 683, 691, 701, 709, 719, 727, 733,
+    739, 743, 751, 757, 761, 769, 773, 787, 797, 809,
+    811, 821, 823, 827, 829, 839, 853, 857, 859, 863,
+    877, 881, 883, 887, 907, 911, 919, 929, 937, 941,
+    947, 953, 967, 971, 977, 983, 991, 997);
+
+
 generic function Resolve<T>(value: T; rest: array of T): T;
 
 { These functions generate random values. }
@@ -20,25 +41,19 @@ generic function GenOneOf<T>(values: array of T): specialize TCheckFunc<T>;
 generic function GenOneOf<T>(values: array of T; weights: array of Double): specialize TCheckFunc<T>;
 generic function GenSequence<T>(values: array of T): specialize TCheckFunc<T>;
 generic function GenInteger<Integer>: specialize TCheckFunc<Integer>;
-generic function GenInteger<Integer>(I, J: Integer): specialize TCheckFunc<Integer>;
+generic function GenInteger<Integer>(I: Integer; J: Integer): specialize TCheckFunc<Integer>;
 // generic function GenCharacter<Char>: specialize TCheckFunc<Char>;
 generic function GenCharacter<Char>(i: String): specialize TCheckFunc<Char>;
 // generic function GenArray<T>(First: array of T; fillValue: T): specialize TCheckFunc<array<T>>;
 generic function GenString<T>(parameters: array of String): specialize TCheckFunc<String>;
 generic function GenAny<T>(): specialize TCheckFunc<T>;
 generic function GenObject<T>(makeObjectFunc: specialize TMakeObjectFunc<T>): specialize TCheckFunc<T>;
-sfsfd
+
 implementation
 
 generic function Resolve<T>(value: T; rest: array of T): T;
 begin
   Result := value;
-  TThread.CreateAnonymousThread(
-    procedure
-    begin
-      f('goroutine');
-    end
-                               ).Start;
 end;
 
 generic function GenLiteral<T>(value: T): specialize TCheckFunc<T>;
@@ -59,21 +74,21 @@ begin
 end;
 
 generic function GenNumber<Integer>(fromNum: Integer = 1; toNum: Integer = 0): specialize TCheckFunc<Integer>;
+var
+  temp: Integer;
+  difference: Integer;
 begin
-  Result := function(): Integer
-  var
-    temp: Integer;
-    difference: Integer;
-  begin
-      if fromNum > toNum then
+  if fromNum > toNum then
       begin
         temp := fromNum;
         fromNum := toNum;
         toNum := temp;
       end;
       difference := toNum - fromNum;
-      Randomize;
-      Result := Round(Random * 10) * difference + fromNum;
+
+  Result := function(): Integer
+  begin
+      Result := Round(Random * toNum) - fromNum;
     end;
 end;
 
@@ -84,7 +99,6 @@ begin
     randNum: Integer;
     index: Integer;
   begin
-    Randomize;
     randNum := Round(Random * 100);
     if randNum >= Length(primes) then
     begin
@@ -161,7 +175,7 @@ begin
   Result := specialize GenOneOf<Integer>(primes);
 end;
 
-generic function GenInteger<Integer>(I, J: Integer): specialize TCheckFunc<Integer>;
+generic function GenInteger<Integer>(I: Integer; J: Integer): specialize TCheckFunc<Integer>;
 var
   temp: Integer;
 begin
